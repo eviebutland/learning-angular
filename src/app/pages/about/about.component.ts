789bf93134316a1960f348f1aa33d40d9ca60df0
Evie Butland <evie.butland@wearehectare.com>
2025-07-31 07:54:12 +0100

    Make a custom pipe

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { DateComponent } from '../../date/date.component';
 import { CurrencyComponent } from '../../currency/currency.component';
+import { ReversePipe } from '../../pipes/reverse.pipe';
+
 
 @Component({
   selector: 'app-about',
-  imports: [DateComponent, CurrencyComponent],
+  imports: [DateComponent, CurrencyComponent, ReversePipe],
   templateUrl: './about.component.html',
   styleUrl: './about.component.sass'
 })
 
 export class AboutComponent {
-
+  town = 'Witney'
 }
